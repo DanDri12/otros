093896a0aac6f95c53b98f61948da2d7a1847fd9
DanDri12 <133004241+DanDri12@users.noreply.github.com>
2024-04-08 21:48:35 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.querySelector('button').addEventListener('click', ()=>{
+    document.querySelector('.enlaces').classList.toggle('accion');
+});
+
+window.addEventListener("scroll", function(){
+    var efect = document.querySelector("nav");
+    var textNav = document.querySelectorAll("nav ul li a");
+
+    if(window.innerWidth>500){
+        if(window.scrollY>0){
+            efect.style.backgroundColor="rgba(0, 145, 255)";
+            textNav.forEach(t => {
+                t.classList.remove('navegationtext');          
+            });
+        } else{
+            efect.style.backgroundColor="transparent";
+            textNav.forEach(t => {
+                t.classList.add('navegationtext');          
+            });
+        }
+    }
+})
+
